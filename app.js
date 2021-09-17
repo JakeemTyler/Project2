2fe1887621d00174671c9c2355bc5ce96de0e8d0
JakeemTyler <89406034+JakeemTyler@users.noreply.github.com>
2021-09-17 09:23:37 -0400

    Fourth Commit

---
+++ app.js
@@ -0,0 +1,33 @@
+var ele = document.body.querySelector(".box");
+var eleAnswer = document.body.querySelector(".answer");
+
+var question =Number(prompt("Enter your first number"));
+//Checking to see if input is a number
+if(isNaN(question)){
+    ele.innerHTML = "Sorry the input was wrong, please only enter numbers";
+
+}else{
+    ele.innerHTML="";
+}
+var operator=prompt("What is your operator");
+
+var question2 = Number(prompt("Enter your second number"));
+//Checking to see if the input is a number
+if(isNaN(question2)){
+    eleAnswer.innerHTML = "Sorry the input was wrong, please only enter numbers and operators";
+}else{
+    eleAnswer.innerHTML="";
+}
+if(operator==="+"){
+    var result =question+question2;
+}
+if(operator==="-"){
+    var result =question-question2;
+}
+if(operator==="*"){
+    var result =question*question2;
+}
+if(operator==="/"){
+    var result =question/question2;
+}
+eleAnswer.innerHTML="Result is "+result;
